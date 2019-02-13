f8f1ee1c13792b357d728f8e46cc09a5832cdc55
amit singh laspal <alaspal@marketo.com>
2019-02-13 15:04:52 +0530

    first commit

---
+++ nodejsdocker.groovy
@@ -0,0 +1,26 @@
+job('NodeJS Docker example') {
+    scm {
+        git('https://github.com/amitlaspal/dslscripts.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('amit.laspal@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('laspal/nodejs-pipeline')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
